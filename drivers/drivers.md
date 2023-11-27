b32f9519f863ebb0c3881ff51e67d9daf8726e32
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2023-11-27 22:49:23 +0100

    Add the drivers installation script

---
+++ drivers/drivers.md
@@ -0,0 +1,16 @@
+# Drivers
+## Revi hardware drivers
+
+## Drivres list
+Driver list of all hardware components of the PC. The driver are full debloated by manufactory.
+
+| HW | Model |
+| ------ | ------ |
+| CPU | [AMD Ryzen 7 7700X](https://www.elevenforum.com/t/drivers-amd-chipset-raid-3xx-4xx-5xx-6xx-trx40.11377/) |
+| GPU | [NVIDIA RTX 2060](https://www.techpowerup.com/download/techpowerup-nvcleanstall/) |
+| Audio | [Realtek Audio](https://www.elevenforum.com/t/drivers-realtek-usb-audio-intel-5xx-6xx-7xx-amd-5xx-6xx-trx40.11389/) |
+| LAN | [Intel(R) Ethernet Connection (2) I225/6](https://www.elevenforum.com/t/drivers-intel-ethernet-wifi-bluetooth.11383/) |
+| WiFi | [MediaTek 802.11ac WiFi Adapter](https://www.elevenforum.com/t/drivers-amd-mediatek-wifi-bluetooth.11384/) |
+| Bluetooth | [MediaTek Bluetooth Adapter](https://www.elevenforum.com/t/drivers-amd-mediatek-wifi-bluetooth.11384/) |
+
+For more information about other drivers visiti [Elevenforum](https://www.elevenforum.com/t/index-all-my-firmware-drivers-software-threads.11360/).
---
+++ install.cmd
@@ -0,0 +1,48 @@
+::   /$$$$$$$                       /$$        /$$$$$$   /$$$$$$ 
+::  | $$__  $$                     |__/       /$$__  $$ /$$__  $$
+::  | $$  \ $$  /$$$$$$  /$$    /$$ /$$      | $$  \ $$| $$  \__/
+::  | $$$$$$$/ /$$__  $$|  $$  /$$/| $$      | $$  | $$|  $$$$$$ 
+::  | $$__  $$| $$$$$$$$ \  $$/$$/ | $$      | $$  | $$ \____  $$
+::  | $$  \ $$| $$_____/  \  $$$/  | $$      | $$  | $$ /$$  \ $$
+::  | $$  | $$|  $$$$$$$   \  $/   | $$      |  $$$$$$/|  $$$$$$/
+::  |__/  |__/ \_______/    \_/    |__/       \______/  \______/ 
+
+::
+:: Global vars
+
+:: Device Manager (devmgmt.msc)
+set devmgmt="%windir%\system32\devmgmt"
+:: GPU driver path
+set gpu="drivers\gpu.exe"
+:: Audio driver path.
+set audio="drivers\audio\Drivres\Realtek\setup.exe"
+
+:: Allow scripts in CMD
+:: set-executionpolicy remotesigned
+@echo off
+timeout 2 >nul
+regedit.exe /s "C:\Windows\Setup\Scripts\registry\requirments.reg"
+
+::
+:: Install drivers
+::
+
+:: CPU chipset driver
+@echo Installing CPU chipset driver ...
+pause
+"C:\Windows\Setup\Scripts\drivers\cpu.exe"
+
+:: GPU driver
+@echo Installing GPU driver ...
+pause
+"C:\Windows\Setup\Scripts\%gpu%"
+
+:: Audio driver
+@echo Installing audio driver ...
+pause
+"C:\Windows\Setup\Scripts\%audio%"
+
+:: Connection cards drivers
+@echo Installing connection cards drivers (Ethernet, WiFi, Bluetooth) ...
+pause
+%devmgmt%
